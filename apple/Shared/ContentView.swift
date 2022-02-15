76f9abaf4080456a09bef4766facebb2a2b8a61a
Nino Kierulf <ninokierulf+github@gmail.com>
2022-02-15 18:11:29 +0800

    Create multi-platform xcode proj with app icon
    Signed-off-by: Nino Kierulf <ninokierulf+github@gmail.com>

---
+++ apple/Shared/ContentView.swift
@@ -0,0 +1,24 @@
+///**
+/**
+ContentView.swift
+Shared
+
+Created by: Nino Kierulf on 15/2/22
+Copyright (c) 2022 ninokierulf
+
+*/
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ apple/Shared/hexagonApp.swift
@@ -0,0 +1,20 @@
+///**
+/**
+hexagonApp.swift
+Shared
+
+Created by: Nino Kierulf on 15/2/22
+Copyright (c) 2022 ninokierulf
+
+*/
+
+import SwiftUI
+
+@main
+struct hexagonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
