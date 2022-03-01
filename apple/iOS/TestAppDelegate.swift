9bfeaa39606edc1901ac3b2980dd0c9dc3531aed
Nino Kierulf <ninokierulf+github@gmail.com>
2022-03-01 11:22:07 +0800

    Setup app delegate with scenes and testing

---
+++ apple/iOS/TestAppDelegate.swift
@@ -0,0 +1,12 @@
+import Foundation
+import UIKit
+
+class TestAppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+
+        print("\(#file)")
+        return true
+    }
+}
